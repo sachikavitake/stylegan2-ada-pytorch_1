1e3745fd795c681790f9722307517ec9ede09964
sachikavitake <74591026+sachikavitake@users.noreply.github.com>
2022-12-01 12:11:31 +0530

    try

--- train.py
+++ train.py
@@ -538,6 +538,7 @@ def main(ctx, outdir, dry_run, **config_kwargs):
     prev_run_ids = [re.match(r'^\d+', x) for x in prev_run_dirs]
     prev_run_ids = [int(x.group()) for x in prev_run_ids if x is not None]
     cur_run_id = max(prev_run_ids, default=-1) + 1
+    print(cur_run_id)
     args.run_dir = os.path.join(outdir, f'{cur_run_id:05d}-{run_desc}')
     assert not os.path.exists(args.run_dir)
 
